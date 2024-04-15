954953f75530b980d6c2ab10b52f17f962ae9e7a
ArchanaLomate <166153960+ArchanaLomate@users.noreply.github.com>
2024-04-15 21:06:22 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
           <p className="m-0 max-w-[30ch] text-sm opacity-50">Challenge Solved</p>
         </a>
         <a
-          href="/question/q2"
+          href="/questions/q2"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -77,7 +77,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Q3{" "}
+            Question 3{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
