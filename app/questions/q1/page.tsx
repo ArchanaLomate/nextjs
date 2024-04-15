5b36397c66551b2a7b895c55e1263824bdfd840e
ArchanaLomate <166153960+ArchanaLomate@users.noreply.github.com>
2024-04-15 21:28:09 +0530

    Update page.tsx

--- app/questions/q1/page.tsx
+++ app/questions/q1/page.tsx
@@ -49,11 +49,11 @@ export default function Home() {
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
      
       </div>
-       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-          <div>
+        <div>
          <p>&nbsp;</p>
           <p>&nbsp;</p>
             </div>
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
          <a
           href="/questions/q1"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
