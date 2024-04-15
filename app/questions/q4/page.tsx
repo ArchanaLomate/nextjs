34a3d36c6f43ac379542eb62eeb1327d681e20c3
ArchanaLomate <166153960+ArchanaLomate@users.noreply.github.com>
2024-04-15 14:00:46 +0530

    Update page.tsx

--- app/questions/q4/page.tsx
+++ app/questions/q4/page.tsx
@@ -35,14 +35,20 @@ export default function Home() {
 I'm here to assist you in your decision-making process regarding the selection of a monitoring and logging tool.
       </p>
         <p>
-Although this topic may have been discussed during the sales cycle, I want to ensure you're aware that Vercel also offers a similar capability. Here are some advantages of using Vercel's in-house tool:
+Although this topic may have been discussed during the sales cycle, I want to ensure you're aware that Vercel also offers a similar capability. 
+           <p>&nbsp;</p>
+          Here are some advantages of using Vercel's in-house tool:
         </p>
+         <p>&nbsp;</p>
 <p>
 1. Vercel's observability tools are seamlessly integrated with our platform, providing tailored monitoring and logging specifically designed for Vercel deployments.
  </p> 
+    <p>&nbsp;</p>
  <p>       
 2. With Vercel, there are no additional expenses for utilizing our observability tools, as they are included as part of your Vercel subscription. This ensures a transparent and predictable pricing structure.
 </p>
+         <p>&nbsp;</p>
+         <p>&nbsp;</p>
   <p>      
    To better understand your requirements and showcase our capabilities, I suggest we engage in a detailed conversation with our technical team. Are there any specific priorities you have in mind for the integration, or are there any tools you're currently evaluating?
 Looking forward to assisting you further and ensuring we find the best solution for your needs.	
