063546089dfb8ec4abdb2693de570e58fa692502
ArchanaLomate <166153960+ArchanaLomate@users.noreply.github.com>
2024-04-15 20:09:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -40,10 +40,22 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+          <a
+          href="/questions/q1"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <h2 className="mb-3 text-2xl font-semibold">
-            Question 1
+            Question 1 {" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
           </h2>
-        <a href="/questions/q1"> Challenge Solved </a>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Challenge Solved
+          </p>
+        </a>
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
